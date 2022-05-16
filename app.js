ac7ab4140bb78299aef1a6dd32daa33c4967ae84
David <Rigby335@hotmail.com>
2022-05-16 16:36:04 -0700

    added event listeners to home page

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 // import functions and grab DOM elements
-
+const signInButton = document.getElementById('sign-in');
+const createButton = document.getElementById('create');
 // let state
 
 // set event listeners 
+signInButton.addEventListener('click', () => {
+    location.replace('signin-page');
+});
+
+createButton.addEventListener('click', () => {
+    location.replace('create-page');
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
